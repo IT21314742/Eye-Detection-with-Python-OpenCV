c3710178ec794dfa550ae01bfd31fd54a6bed947
IT21314742 <it21314742@my.sliit.lk>
2024-09-07 13:25:25 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier()
+face_cascade = cv2.CascadeClassifier(cv2)
 
 while True:
     ret, frame = cap.read()
