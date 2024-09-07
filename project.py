f6d7c8b507327c917f5bf8610f55f4bad1bd0669
IT21314742 <it21314742@my.sliit.lk>
2024-09-07 17:29:26 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ while True:
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade
+    faces = face_cascade.detectMultiScale
 
     cv2.imshow('frame', image)
 
