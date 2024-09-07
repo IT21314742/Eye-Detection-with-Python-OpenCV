922d14e752b22a083b8289a7241031ab0d9e445c
IT21314742 <it21314742@my.sliit.lk>
2024-09-07 13:26:26 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + )
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "")
 
 while True:
     ret, frame = cap.read()
