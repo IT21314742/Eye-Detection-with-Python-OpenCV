8baf663779b34516957c049108b64caa92167ea1
IT21314742 <it21314742@my.sliit.lk>
2024-09-07 17:26:50 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcasecade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + )
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcasecade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcasecade_frontalface_default.xml')
 
 while True:
     ret, frame = cap.read()
