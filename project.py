799dd3d631ad9b09bc98014e91e46e34bc37dc51
IT21314742 <it21314742@my.sliit.lk>
2024-09-07 13:24:35 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+face_cascade
 
 while True:
     ret, frame = cap.read()
 
     cv2.imshow('frame', image)
 
-    if cv2.waitKey(1) == ord('q')
+    if cv2.waitKey(1) == ord('q'):
         break
 
 
