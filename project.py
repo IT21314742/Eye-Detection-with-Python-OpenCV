4990a469f44afd9cc49e864e44bab7fd1aec185f
IT21314742 <it21314742@my.sliit.lk>
2024-09-07 17:30:06 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -12,7 +12,7 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    cv2
+    cv2.imshow
 
     cv2.imshow('frame', image)
 
